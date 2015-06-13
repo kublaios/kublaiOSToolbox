5bca2c6fd10fa8017647dda90ca7d2abafd10c72
Kubilay Erdogan's MBPR <kubilay.erdogan@frame.com.tr>
2015-06-13 09:32:16 +0300

    3 files for good

---
+++ Pod/Classes/Extensions.swift
@@ -0,0 +1,125 @@
+//
+//  Extensions.swift
+//  kublaiOSToolbox
+//
+//  Created by Kubilay Erdogan on 13/06/15.
+//  Copyright (c) 2015 Kubilay Erdogan
+//
+
+import Foundation
+
+extension String {
+    
+    func floatValue() -> Float {
+        return (self as NSString!).floatValue
+    }
+    
+    func numberValue() -> NSNumber {
+        var numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        return numberFormatter.numberFromString(self) as NSNumber!
+    }
+    
+    func dateValue(format: String?) -> NSDate {
+        var dateFormatter = NSDateFormatter()
+        if let newFormat = format {
+            dateFormatter.dateFormat = newFormat
+        } else {
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        }
+        if let retDate: NSDate = dateFormatter.dateFromString(self) {
+            return retDate
+        } else {
+            return NSDate()
+        }
+    }
+    
+    func boolValue() -> Bool {
+        switch self {
+        case "0","false","FALSE","no","NO","hayir","HAYIR":
+            return false
+        case "1","true","TRUE","yes","YES","evet","EVET":
+            return true
+        default:
+            return false
+        }
+    }
+    
+}
+
+extension NSDate {
+    
+    func stringValue(format: String?) -> String {
+        var dateFormatter = NSDateFormatter()
+        if let newFormat = format {
+            dateFormatter.dateFormat = newFormat
+        } else {
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        }
+        return dateFormatter.stringFromDate(self)
+    }
+    
+}
+
+extension NSDictionary {
+    
+    func isNullOrEmpty() -> Bool {
+        return self.isEqual(NSDictionary())
+    }
+    
+}
+
+extension UILabel {
+    
+    func resize(size: CGFloat) {
+        self.font = self.font.fontWithSize(size)
+    }
+    
+}
+
+extension UIColor {
+    
+    convenience init(rgba: String) {
+        var red:   CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue:  CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        
+        if rgba.hasPrefix("#") {
+            let index   = advance(rgba.startIndex, 1)
+            let hex     = rgba.substringFromIndex(index)
+            let scanner = NSScanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexLongLong(&hexValue) {
+                switch (count(hex)) {
+                case 3:
+                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
+                case 4:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                case 6:
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                case 8:
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                default:
+                    print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+                }
+            } else {
+                println("Scan hex error")
+            }
+        } else {
+            print("Invalid RGB string, missing '#' as prefix")
+        }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+}
---
+++ Pod/Classes/Utilities.swift
@@ -0,0 +1,57 @@
+//
+//  Utilities.swift
+//  kublaiOSToolbox
+//
+//  Created by Kubilay Erdogan on 13/06/15.
+//  Copyright (c) 2015 Kubilay Erdogan
+//
+
+import Foundation
+
+class Utilities {
+    
+    static func JSONDataFromURL(url: String) -> NSDictionary {
+        var err: NSError
+        var errPointer: NSErrorPointer = NSErrorPointer()
+        var request: NSURLRequest = NSURLRequest(URL: NSURL(string: url) as NSURL!)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?>=nil
+        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error:errPointer)!
+        if let retDict: AnyObject = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: errPointer) {
+            return retDict as! NSDictionary
+        } else {
+            return NSDictionary()
+        }
+    }
+    
+    static func formatURL(url: String, param1: String?, param2: String?) -> String {
+        if param1 != nil {
+            if param2 != nil {
+                return NSString(format: url, param1 as NSString!, param2 as NSString!) as String
+            } else {
+                return NSString(format: url, param1 as NSString!) as String
+            }
+        } else {
+            return ""
+        }
+    }
+    
+    static func Log(log: String!) {
+        if true {
+            println(log)
+        }
+    }
+    
+    static func t(string: String) -> String {
+        // TODO: Implement localization
+        return string
+    }
+    
+    static func isHostReachable() -> Bool {
+        return (Reachability(hostName: "google.com").currentReachabilityStatus() != NetworkStatus.NotReachable)
+    }
+    
+    static func viewController(name: String, inStoryboard: UIStoryboard) -> UIViewController {
+        return inStoryboard.instantiateViewControllerWithIdentifier(name) as! UIViewController
+    }
+    
+}
